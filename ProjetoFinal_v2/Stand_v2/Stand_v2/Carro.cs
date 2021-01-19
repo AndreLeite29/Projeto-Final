a237986d8e6fa1680a79ff2f6fca980a6641fc78
André Leite <58446356+AndreLeite29@users.noreply.github.com>
2021-01-19 15:14:56 +0000

    Add files via upload

---
+++ ProjetoFinal_v2/Stand_v2/Stand_v2/Carro.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Stand_v2
+{
+    class Carros                                //Propriedades
+    {
+        public Carros(string name, int miles_per_Gallon, int cylinders, int kilometers, int horsepower, int weight_in_lbs, float acceleration, string year, string origin, int price)
+        {
+            Name = name;
+            Miles_per_Gallon = miles_per_Gallon;
+            Cylinders = cylinders;
+            Kilometers = kilometers;
+            Horsepower = horsepower;
+            Weight_in_lbs = weight_in_lbs;
+            Acceleration = acceleration;
+            Year = year;
+            Origin = origin;
+            Price = price;
+        }
+
+        public string Name { get; set; }
+
+        public int Miles_per_Gallon { get; set; }
+
+        public int Cylinders { get; set; }
+
+        public int Kilometers { get; set; }
+
+        public int Horsepower { get; set; }
+
+        public int Weight_in_lbs { get; set; }
+
+        public float Acceleration { get; set; }
+
+        public string Year { get; set; }
+
+        public string Origin { get; set; }
+
+        public int Price { get; set; }
+    }
+}
